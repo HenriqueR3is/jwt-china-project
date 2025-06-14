a9d357e011b6ffe2398ab1327fce057bce1ca5e9
Henrique <henriquehkreis@gmail.com>
2025-06-14 18:10:38 -0300

    sec commit

---
+++ src/main/java/br/com/faculdade/jwt_china_project/JwtChinaProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.faculdade.jwt_china_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtChinaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtChinaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/faculdade/jwt_china_project/JwtChinaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.faculdade.jwt_china_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtChinaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
