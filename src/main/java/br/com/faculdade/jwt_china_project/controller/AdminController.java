174d574807ed47992cff162e70a4218cf54b4919
Henrique <henriquehkreis@gmail.com>
2025-06-14 18:49:15 -0300

    feat: Add user and admin management endpoints

---
+++ src/main/java/br/com/faculdade/jwt_china_project/controller/AdminController.java
@@ -0,0 +1,25 @@
+package br.com.faculdade.jwt_china_project.controller;
+
+import br.com.faculdade.jwt_china_project.model.User;
+import br.com.faculdade.jwt_china_project.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
+    }
+}
